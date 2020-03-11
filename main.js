74e9b73742bc230980dd53cf86bbcb3b1e186859
JessicaThi <jessica.thielemans@hetic.net>
2020-03-11 12:09:40 +0100

    create folder images

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Mac Comestics - Pony Park 포니 ",
-    image: "beck-and-call.png",
+    image: "images/beck-and-call.png",
     lipsticks: [
       {
         lipstickId: 1,
         lipstickColor: "#df7493",
         lipstickName: "Beck and Call",
-        lipstickImage: "beck-and-call.png"
+        lipstickImage: "imageS/beck-and-call.png"
       },
       {
         lipstickId: 2,
         lipstickColor: "#e45e78",
         lipstickName: "Cordon Bleu",
-        lipstickImage: "cordon-bleu.png"
+        lipstickImage: "images/cordon-bleu.png"
       },
       {
         lipstickId: 3,
         lipstickColor: "#ed689c",
         lipstickName: "Love is Blind",
-        lipstickImage: "love-is-blind.png"
+        lipstickImage: "images/love-is-blind.png"
       }
     ],
     cart: 0
