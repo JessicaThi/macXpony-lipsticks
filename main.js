a12d4d3428eb3ba24f6e46637936de30e40c4069
JessicaThi <jessica.thielemans@hetic.net>
2020-03-11 13:03:29 +0100

    refactor code for add computed propreties

--- main.js
+++ main.js
@@ -3,37 +3,53 @@
 var app = new Vue({
   el: "#app",
   data: {
-    product: "Mac Comestics - Pony Park 포니 ",
-    image: "images/beck-and-call.png",
+    brand: "Mac Comestics - Pony Park 포니",
+    product: "Lipsticks",
+    selectedLipstick: 0,
     lipsticks: [
       {
         lipstickId: 1,
         lipstickColor: "#df7493",
         lipstickName: "Beck and Call",
-        lipstickImage: "imageS/beck-and-call.png"
+        lipstickImage: "imageS/beck-and-call.png",
+        lipstickQuantity: 20
       },
       {
         lipstickId: 2,
         lipstickColor: "#e45e78",
         lipstickName: "Cordon Bleu",
-        lipstickImage: "images/cordon-bleu.png"
+        lipstickImage: "images/cordon-bleu.png",
+        lipstickQuantity: 0
       },
       {
         lipstickId: 3,
         lipstickColor: "#ed689c",
         lipstickName: "Love is Blind",
-        lipstickImage: "images/love-is-blind.png"
+        lipstickImage: "images/love-is-blind.png",
+        lipstickQuantity: 11
       }
     ],
     cart: 0
   },
   methods: {
-    updateProduct(lipstickImage) {
-      this.image = lipstickImage;
+    updateProduct(index) {
+      this.selectedLipstick = index;
+      console.log(index);
     },
 
     addToCart() {
       this.cart += 1;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.lipsticks[this.selectedLipstick].lipstickImage;
+    },
+    inStock() {
+      return this.lipsticks[this.selectedLipstick].lipstickQuantity;
     }
   }
 });
